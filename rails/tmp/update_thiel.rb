caaf707af5251b64a7fc8d1ee6970d761c600427
Kody Kendall <kodyckendall@gmail.com>
2026-05-12 14:32:06 -0600

    Added images to Controversial History

---
+++ rails/tmp/update_thiel.rb
@@ -0,0 +1,25 @@
+thiel = Controversy.find_by(name: 'Peter Thiel')
+thiel.update!(
+  bio_heading: 'Peter Thiel',
+  bio: "Peter Thiel grew up playing chess and memorizing geography. As his family moved between countries, he attended seven different elementary schools and had nearly no friends until he approached his teens (Packer 120). He hated the strictness of his teachers, but got near-perfect scores. When his family moved to the San Francisco Bay Area, he was thrust into the underfunded California school system (121). He coped with the chaos of uncontrollable classrooms by obsessing over his grades (122). Like a certain set of high-achieving boys, he was into Tolkien, Sci-Fi, math, and computers, along with the libertarianism that this logic-based mindset cultivated (123). Thiel graduated high school as valedictorian, feeling “very optimistic.” He had no specific ambition, simply “to somehow have an impact on the world” (124).
+
+At Stanford, Thiel made friends by debating theories of property late into the night (124). He co-founded The Stanford Review to push back against the university, which was moving to the left (125). He went on to practice law and then finance for a few years, but found the engineered competitiveness pointless (127). Thiel moved back to California and the region that had become known as Silicon Valley. He started his own hedge fund and befriended Max Levchin (131). Together, they created Confinity, which used PayPal to transfer money between Palm Pilot devices (132). Confinity grew to a million users, and Thiel hoped it would further his libertarian ideals by freeing people from government finance policy (133). Just before the dot-com bubble burst, he secured a round of funding and merged with Elon Musk’s X.com payment company (134). The company survived and, when sold, made Thiel $55 million (135).
+
+Thiel’s friend Hoffman founded LinkedIn after the PayPal sale, through which he connected Thiel to Mark Zuckerberg and Thefacebook. In 2004, Thiel invested $500,000 into Thefacebook, later earning him $1.5 billion (210). The same year, Thiel cofounded Palantir, which analyzed data to help governments track down terrorists and would later be valued at $2.5 billion. Thiel was becoming “one of the world’s most successful technology investors,” and his new fund, Clarium Capital, skyrocketed (211). He rented mansions and bought racecars, though he often continued to wear T-shirts (213).
+
+In 2008, just after moving the Clarium offices to Manhattan to be closer to Wall Street, the financial markets collapsed. “With everyone else in a panic, Thiel tried to catch a falling knife, but this time contrarianism became his enemy.” He had predicted a housing crash for years, yet timed his response disastrously, buying before stocks fell, and selling before they rose. He lost money and his investors pulled out. Clarium moved back to San Francisco. To Thiel’s credit, “he took losing well and kept an even keel with his staff.” However, “his view of America began to darken” (215).
+
+In 2009, Thiel saw the last four decades as two steps forward and two steps back, but with key institutions eroding the entire time (381). Looking back, he was unimpressed with computer innovation, pointing to the iPhone and saying, “I don’t consider this to be a technological breakthrough” (383). He believed progress in finance and computers came from the lack of regulation (386). He gave $2.6 million to Ron Paul’s Super PAC, and smaller amounts to other causes. “But more and more he wanted to get away from politics, a highly inefficient way to bring about change” (387).
+
+Thiel refused to submit to what he called “the ideology of the inevitability of the death of every individual” (388). He approached death as a problem to be solved, and, in 2010, invested in Halcyon, a startup working to sequence the human genome. Aside from health, he also invested in AI, space exploration, and independent city-states on the ocean (390). He disliked the inequality of privately-funded elementary schools and saw the value of higher education as dubious because it lacked entrepreneurial training (392-393). He started the Thiel Fellowships: grants given to young people to forgo college and start their own tech businesses. He prioritized those that would attract serious talent (394). Always, he was looking for something with a scope grand enough to end the tech-slowdown (395). He was no longer a hedge fund titan, but he was becoming “the intellectual provocateur he had dreamed of being at Stanford” (386).
+
+Work Cited: The Unwinding: An Inner History of the New America, by George Packer. Published by Farrar, Straus and Giroux, 2014.",
+  contributions: nil,
+  contributions_heading: nil,
+  main_ideas: nil,
+  main_ideas_heading: nil,
+  legacy: nil,
+  legacy_heading: nil,
+  tagline: 'The venture capitalist who redefined the relationship between technology and politics.'
+)
+puts "Updated Peter Thiel"
